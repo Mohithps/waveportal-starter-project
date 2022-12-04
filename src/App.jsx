5681c5d1d85347f1595ced43d400f60bc8c79a31
Mohithps <>
2022-12-04 05:55:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there.....!
+        </div>
+
+        <div className="bio">
+        I am mohith p shetty a student ,intrested in games and web3 pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
